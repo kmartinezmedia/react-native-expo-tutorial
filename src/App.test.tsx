e8fb8176247f88ef4383efdf63fd5fe3d6dbe2f1
Katherine Martinez <kmartinezmedia@gmail.com>
2020-02-16 21:06:15 -0800

    Fix yarn.lock duplicate react deps

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,13 @@
 import "react-native";
-import * as React from "react";
-import * as renderer from "react-test-renderer";
+import React from "react";
+import renderer, { act } from "react-test-renderer";
 
 import { App } from "./App";
 
 // Note: test renderer must be required after react-native.
 
 it("renders correctly", () => {
-  renderer.act(() => {
+  act(() => {
     renderer.create(<App />);
   });
 });
